8f7757cb8427f139ee74cee886fc0af045728574
Sheiny Fabre <sheiny.fabre@gmail.com>
2017-10-20 15:39:19 -0200

    simplified some methods.

--- final/prototype/version_1.py
+++ final/prototype/version_1.py
@@ -1,6 +1,10 @@
 from random import randint
 import sys
 
+def sanity_check(containers, container_size):
+    if len([ x for x in containers if x > container_size]):
+        print("Deu ruim / give bad")
+
 def check_result(container, container_size):
     print("=====================")
     print("Number of used containers: ", len(container))
@@ -10,89 +14,69 @@ def check_result(container, container_size):
     print("total - occupied/free (usage%): ", total_space," - ", occupied_space, "/",  total_space - occupied_space, " (", "%.2f" % (100*occupied_space/total_space) ,")" )
     print("=====================\n")
 
-
-def first_fit_unordered(obects, container, container_size):
-    container.insert(len(container), 0)
-    bottom = 0
-    top = 0
-    for obj in objects[:]:
-        fit_container = bottom 
-        for cont in container[bottom:top+1]:
-            if cont + obj > container_size:
-                fit_container+=1
-            else:
+def first_fit(objects, container_size):
+    containers = []
+    for obj in objects:
+        choosen_container_index = 0
+        for c_index in range(0,len(containers)):
+            if containers[c_index] + obj <= container_size:
                 break
+            else:
+                choosen_container_index +=1
+        if choosen_container_index < len(containers):
+            containers[choosen_container_index] += obj
+        else:
+            containers.append(obj)
+    return containers
 
-        if fit_container == len(container):
-            container.insert(len(container), 0)
-            top+=1
+def first_fit_ascending_sort(objects, container_size):
+    objects.sort()
+    return first_fit(objects, container_size)
 
-        container[fit_container] += obj
-
-        if container[fit_container] == container_size :
-            container[bottom], container[fit_container] = container[fit_container], container[bottom]
-            bottom+=1
-
-def first_fit_ordered(obects, container, container_size):
-    container.insert(len(container), 0)
-    for obj in objects[:]:
-        if container[len(container)-1] + obj > container_size:
-            container.insert(len(container), 0)
-        
-        container[len(container)-1] += obj
-        
-def biggest_n_smallest(obects, container, container_size):
-    smallest = 0
-    biggest = len(objects)-1
-
-    while smallest <= biggest :
-        container.insert(len(container), 0)
-        while smallest <= biggest and objects[biggest] + container[len(container)-1] <= container_size :
-            container[len(container)-1] += objects[biggest]
-            biggest-=1
-
-        while smallest <= biggest and objects[smallest] + container[len(container)-1] <= container_size :
-            container[len(container)-1] += objects[smallest]
-            smallest+=1
-            
-# = first_fit_ordered
-# def smallest_n_biggest(obects, container, container_size):
-#   smallest = 0
-#   biggest = len(objects)-1
-
-#   while smallest <= biggest :
-#     container.insert(len(container), 0)
-
-#     while smallest <= biggest and objects[smallest] + container[len(container)-1] <= container_size :
-#       container[len(container)-1] += objects[smallest]
-#       smallest+=1
-
-#     while smallest <= biggest and objects[biggest] + container[len(container)-1] <= container_size :
-#       container[len(container)-1] += objects[biggest]
-#       biggest-=1
+def first_fit_descending_sort(objects, container_size):
+    objects.sort(reverse=True)
+    return first_fit(objects, container_size)
 
 
-n = 1000
-container_size = 256
-container = []
-objects = [ randint(1, container_size) for i in range(n) ]
-print("Cargo size: ", sum(objects), "\n")
-print("first_fit_unordered")
-first_fit_unordered(objects, container, container_size)
-check_result(container, container_size)
+def biggest_n_smallest(objects, container_size):
+    objects.sort()
+    containers = []
+    smallest_index = 0
+    biggest_index = len(objects)-1
 
-container = []
-objects.sort()
-print("first_fit_ordered")
-first_fit_ordered(objects, container, container_size)
-check_result(container, container_size)
+    while smallest_index <= biggest_index :
+        container = 0
+        while smallest_index <= biggest_index and objects[biggest_index] + container <= container_size :
+            container += objects[biggest_index]
+            biggest_index -= 1
 
-container = []
-print("biggest_n_smallest")
-biggest_n_smallest(objects, container, container_size)
-check_result(container, container_size)
+        while smallest_index <= biggest_index and objects[smallest_index] + container <= container_size :
+            container += objects[smallest_index]
+            smallest_index += 1
+        containers.append(container)
+    return containers
 
-# container = []
-# print("smallest_n_biggest")
-# smallest_n_biggest(objects, container, container_size)
-# check_result(container, container_size)
+
+n_objects = 100
+container_size = 1000
+objects = [ randint(1, container_size) for i in range(n_objects) ]
+
+print("first_fit:")
+ff_containers = first_fit(objects[:], container_size)
+sanity_check(ff_containers, container_size)
+check_result(ff_containers, container_size)
+
+print("first_fit_ascending_sort:")
+ff_as_ord_containers = first_fit_ascending_sort(objects[:], container_size)
+sanity_check(ff_as_ord_containers, container_size)
+check_result(ff_as_ord_containers, container_size)
+
+print("first_fit_descending_sort:")
+ff_des_ord_containers = first_fit_descending_sort(objects[:], container_size)
+sanity_check(ff_des_ord_containers, container_size)
+check_result(ff_des_ord_containers, container_size)
+
+print("biggest_n_smallest:")
+bs_containers = biggest_n_smallest(objects[:], container_size)
+sanity_check(bs_containers, container_size)
+check_result(bs_containers, container_size)
